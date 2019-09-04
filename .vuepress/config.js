4497f315db1e9ae07915a7c82445ca2ee45b26ec
Ryosuke Uchitate <uchitate.r@waja.jp>
2019-09-05 01:02:24 +0900

    Navbar

---
+++ .vuepress/config.js
@@ -0,0 +1,12 @@
+module.exports = {
+	themeConfig: {
+		logo: '/assets/img/jjug_logo.png',
+		nav: [
+			{ text: 'Home', link: '/' },
+			{ text: 'スポンサー', link: '/sponsors/' },
+			{ text: 'セッション一覧', link: '/sessions/' },
+		],
+		search: false,
+		searchMaxSuggestions: 10
+	}
+};
