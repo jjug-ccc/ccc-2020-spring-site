b8e9403b96b914630973493eed635aff4234b9b7
Ryosuke Uchitate <uchitate.r@waja.jp>
2019-09-05 22:29:04 +0900

    footer

--- .vuepress/components/Footer.vue
+++ .vuepress/components/Footer.vue
@@ -1,27 +1,29 @@
 <template>
-	<div class="container">
-		<div class="row">
-			<div class="p-4 col-md-7">
-				<h3>個人情報の取り扱い</h3>
-				<p class="my-3"> お申込戴きました情報は個人を特定できる状態で、本団体以外の第三者に開示することはございません。 (法律により開示が要求された場合、または他のお客様、あるいはイベントに関係する団体・個人の権利などを守る場合に必要という判断が合理的にされる場合を除きます) </p>
+	<footer class="page-footer">
+		<div class="container">
+			<div class="row">
+				<div class="p-4 col-md-7">
+					<h3>個人情報の取り扱い</h3>
+					<p class="my-3"> お申込戴きました情報は個人を特定できる状態で、本団体以外の第三者に開示することはございません。 (法律により開示が要求された場合、または他のお客様、あるいはイベントに関係する団体・個人の権利などを守る場合に必要という判断が合理的にされる場合を除きます) </p>
+				</div>
+				<div class="p-4 col-md-5">
+					<h3>About Us</h3>
+					<p>
+						<a href="http://www.java-users.jp/?page_id=7" target="_blank">JJUGについて</a>
+					</p>
+					<p>
+						<a href="http://www.java-users.jp/?page_id=18" target="_blank">入会案内</a>
+					</p>
+					<p>
+						<a href="http://www.java-users.jp/?page_id=166" target="_blank">幹事会メンバー</a>
+					</p>
+				</div>
 			</div>
-			<div class="p-4 col-md-5">
-				<h3>About Us</h3>
-				<p>
-					<a href="http://www.java-users.jp/?page_id=7" target="_blank">JJUGについて</a>
-				</p>
-				<p>
-					<a href="http://www.java-users.jp/?page_id=18" target="_blank">入会案内</a>
-				</p>
-				<p>
-					<a href="http://www.java-users.jp/?page_id=166" target="_blank">幹事会メンバー</a>
-				</p>
+			<div class="row">
+				<div class="col-md-12 mt-3">
+					<p class="footer-copyright text-center">© Copyright 2019 JJUG - All rights reserved. </p>
+				</div>
 			</div>
 		</div>
-		<div class="row">
-			<div class="col-md-12 mt-3">
-				<p class="text-center">© Copyright 2019 JJUG - All rights reserved. </p>
-			</div>
-		</div>
-	</div>
+	</footer>
 </template>
