349e813af9702f3cd867193c4c1e942e1cc0405e
Ryosuke Uchitate <uchitate.r@waja.jp>
2019-09-05 20:02:00 +0900

    Footer修正

--- .vuepress/components/Footer.vue
+++ .vuepress/components/Footer.vue
@@ -1,47 +1,27 @@
 <template>
-	<div class="text-white bg-dark">
-		<div class="container">
-			<div class="row">
-				<div class="p-4 col-md-7">
-					<h3 class="text-secondary">個人情報の取り扱い</h3>
-					<p class="my-3"> お申込戴きました情報は個人を特定できる状態で、本団体以外の第三者に開示することはございません。 (法律により開示が要求された場合、または他のお客様、あるいはイベントに関係する団体・個人の権利などを守る場合に必要という判断が合理的にされる場合を除きます) </p>
-				</div>
-				<div class="p-4 col-md-2">
-					<h3 class="text-secondary">About Us</h3>
-					<p>
-						<a href="http://www.java-users.jp/?page_id=7" class="text-white" target="_blank">JJUGについて</a>
-					</p>
-					<p>
-						<a href="http://www.java-users.jp/?page_id=18" class="text-white" target="_blank">入会案内</a>
-					</p>
-					<p>
-						<a href="http://www.java-users.jp/?page_id=166" class="text-white" target="_blank">幹事会メンバー</a>
-					</p>
-				</div>
-				<div class="p-4 col-md-3">
-					<h3 class="text-secondary">Follow</h3>
-					<a href="https://www.facebook.com/japanjug/" target="blank">
-						<i class="fa fa-facebook-square text-muted m-2 fa-2x"></i>
-					</a>
-					<a href="https://twitter.com/jjug" target="blank">
-						<i class="fa fa-2x fa-twitter m-2 text-muted"></i>
-					</a>
-					<a href="https://plus.google.com/+JavausersJp00" target="blank">
-						<i class="fa fa-2x fa-google-plus-official text-muted m-2"></i>
-					</a>
-				</div>
+	<div class="container">
+		<div class="row">
+			<div class="p-4 col-md-7">
+				<h3>個人情報の取り扱い</h3>
+				<p class="my-3"> お申込戴きました情報は個人を特定できる状態で、本団体以外の第三者に開示することはございません。 (法律により開示が要求された場合、または他のお客様、あるいはイベントに関係する団体・個人の権利などを守る場合に必要という判断が合理的にされる場合を除きます) </p>
 			</div>
-			<div class="row">
-				<div class="col-md-12 mt-3">
-					<p class="text-center">© Copyright 2019 JJUG - All rights reserved. </p>
-				</div>
+			<div class="p-4 col-md-5">
+				<h3>About Us</h3>
+				<p>
+					<a href="http://www.java-users.jp/?page_id=7" target="_blank">JJUGについて</a>
+				</p>
+				<p>
+					<a href="http://www.java-users.jp/?page_id=18" target="_blank">入会案内</a>
+				</p>
+				<p>
+					<a href="http://www.java-users.jp/?page_id=166" target="_blank">幹事会メンバー</a>
+				</p>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-md-12 mt-3">
+				<p class="text-center">© Copyright 2019 JJUG - All rights reserved. </p>
 			</div>
 		</div>
 	</div>
 </template>
-
-<script>
-	export default {
-		name: "Footer"
-	}
-</script>
