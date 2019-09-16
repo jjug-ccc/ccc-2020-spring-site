2f9d72a4323496c1e5e67a03271fda92c62b85df
Ryosuke Uchitate <uchitate.r@waja.jp>
2019-09-16 22:19:57 +0900

    セッション一覧のリンク修正

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -7,7 +7,7 @@ module.exports = {
 		nav: [
 			{ text: 'Home', link: '/' },
 			{ text: 'スポンサー', link: '/sponsors/' },
-			{ text: 'セッション一覧', link: '/sessions/' },
+			{ text: 'セッション一覧', link: 'https://jjug-cfp.cfapps.io/conferences/4abde357-9994-4578-b760-a1698ff6f338' },
 		],
 		search: false,
 		searchMaxSuggestions: 10
