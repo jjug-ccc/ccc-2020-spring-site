5c0ac11096a42026eb19a8c9dff5fb1d76998ffa
Ryosuke Uchitate <ry0suke.uch1tate@gmail.com>
2020-03-22 16:57:24 +0900

    見た目修正

--- src/components/AboutJjug/index.js
+++ src/components/AboutJjug/index.js
@@ -2,10 +2,9 @@ import React from 'react'
 
 const AboutJjug = () => (
   <div>
-    <div>
-      <p>日本Javaユーザーグループ（Japan Java User Group/JJUG）は9000人以上の Javaエンジニアが参加する日本最大のJavaコミュニティです。以下のような活動を行っています。</p>
-      <p>The Japan Java User Group (JJUG) is the largest Java community in Japan with over 9000 Java engineers. The following activities are conducted.</p>
-    </div>
+    <p>日本Javaユーザーグループ（Japan Java User Group/JJUG）は9000人以上の Javaエンジニアが参加する日本最大のJavaコミュニティです。以下のような活動を行っています。</p>
+    <p>The Japan Java User Group (JJUG) is the largest Java community in Japan with over 9000 Java engineers. The following activities are conducted.</p>
+
     <dl>
       <dt>ナイトセミナー / Night Seminar</dt>
       <dd>毎月1回夜に開催する勉強会 / Seminar held once a month</dd>
@@ -16,12 +15,11 @@ const AboutJjug = () => (
       <dt>国際会議参加 / Participation in international conferences</dt>
       <dd>世界中のJUGとの交流 / Making a relationship with international Java Communities</dd>
     </dl>
-    <div>
-      <p>
-        詳しくはJJUGの<a href="http://www.java-users.jp/" target="blank" rel="noopener">オフィシャルサイト</a>や<a href="https://jjug.doorkeeper.jp/" target="blank" rel="noopener">コミュニティページ（DoorKeeper）</a>を確認してください。 /
-        Please check JJUG <a href="http://www.java-users.jp/" target="blank" rel="noopener">official website</a> and <a href="https://jjug.doorkeeper.jp/" target="blank" rel="noopener">community page (DoorKeeper)</a> for more detail information.
-      </p>
-    </div>
+
+    <p>
+      詳しくはJJUGの<a href="http://www.java-users.jp/" target="blank" rel="noopener">オフィシャルサイト</a>や<a href="https://jjug.doorkeeper.jp/" target="blank" rel="noopener">コミュニティページ（DoorKeeper）</a>を確認してください。 /
+      Please check JJUG <a href="http://www.java-users.jp/" target="blank" rel="noopener">official website</a> and <a href="https://jjug.doorkeeper.jp/" target="blank" rel="noopener">community page (DoorKeeper)</a> for more detail information.
+    </p>
   </div>
 );
 
--- src/components/CccDescription/index.js
+++ src/components/CccDescription/index.js
@@ -2,11 +2,10 @@ import React from 'react'
 
 const CccDescription = () => (
     <div>
-        <div>
-            <p>JJUG CCCは毎年2回、春と秋に開催する日本最大のJavaコミュニティイベントです。Java関連の技術や事例に関する良質なセッションが行われ、また異なる分野で活躍するJava技術者が一堂に会する場ともなっています。ぜひご参加ください。</p>
-            <p>JJUG CCC is the largest Java community event organized by Japan Java User Group (JJUG), held twice a year in spring and autumn. It provides great sessions of Java-related technologies and practices, and brings together Java engineers from different disciplines.</p>
-        </div>
-        <div>
+      <p>JJUG CCCは毎年2回、春と秋に開催する日本最大のJavaコミュニティイベントです。Java関連の技術や事例に関する良質なセッションが行われ、また異なる分野で活躍するJava技術者が一堂に会する場ともなっています。ぜひご参加ください。</p>
+      <p>JJUG CCC is the largest Java community event organized by Japan Java User Group (JJUG), held twice a year in spring and autumn. It provides great sessions of Java-related technologies and practices, and brings together Java engineers from different disciplines.</p>
+
+      <dl>
             <dt>主催 / Organizer</dt>
             <dd>日本Javaユーザグループ / Japan Java User Group</dd>
             <dt>Japan Java User Group</dt>
@@ -18,7 +17,7 @@ const CccDescription = () => (
             <dd>無料 / Free</dd>
             <dt>ハッシュタグ / Hashtag</dt>
             <dd><a href="https://twitter.com/search?q=%23jjug_ccc" target="blank" rel="noopener">#jjug_ccc</a>（セッションごとのハッシュタグあり / We will also use separate hashtags for each session）</dd>
-        </div>
+      </dl>
     </div>
 );
 
